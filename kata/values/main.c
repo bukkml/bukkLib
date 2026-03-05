0ad04e62519261dbe723ec8b2aa6fe343da86d9c
bukk <mak.kurlovich@gmail.com>
2026-03-05 12:22:42 +0700

    kata: values

--- kata/values/main.c
+++ kata/values/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <float.h>
 #include <limits.h>
+#include <math.h>
 
 int main() {
     //NOTE: VALUES TAKEN BY <limits.h> AND <float.h>
@@ -17,25 +18,39 @@ int main() {
     printf("\nSigned short MIN = %d\tMAX = %d\n", SHRT_MIN, SHRT_MAX);
     printf("Unsigned short MIN = %d\tMAX = %d\n", 0, USHRT_MAX);
 
-    //NOTE: LONG VALUES
+    //NOTE: LONG INT VALUES
     printf("\nSigned long MIN = %d\tMAX = %d\n", LONG_MIN, LONG_MAX);
     printf("Unsigned long MIN = %d\tMAX = %d\n", 0, ULONG_MAX);
 
     //NOTE: FLOAT VALUES
     printf("\nSigned float MIN = %d\tMAX = %d\n", FLT_MIN, FLT_MAX);
-    printf("Unsigned float MIN = %d\tMAX = %d\n", 0, ); //WARN: UNSIGNED FLOAT ???
 
     //NOTE: DOUBLE VALUES
     printf("\nSigned double MIN = %d\tMAX = %d\n", DBL_MIN, DBL_MAX);
-    printf("Unsigned double MIN = %d\tMAX = %d\n", 0, ); //WARN: UNSIGNED DOUBLE ???
 
     //NOTE: LONG DOUBLE VALUES
-    printf("\nSigned long dooble MIN = %d\t MAX %d\n", LDBL_MIN, LDBL_MAX);
-    printf("Unsigned long double MIN = %d\tMAX = %d\n", 0, ); //WARN: UNSIGNED LONG DOUBLE ???
+    printf("\nSigned long dooble MIN = %d\tMAX %d\n", LDBL_MIN, LDBL_MAX);
 
     //NOTE: LONG LONG VALUES
     printf("\nSigned long long MIN = %d\tMAX = %d\n", LONG_LONG_MIN, LONG_LONG_MAX);
     printf("Unsigned long long MIN = %d\tMAX = %d\n", 0, ULONG_MAX);
 
+    //NOTE: VALUES TAKEN BY MATH
+
+    //NOTE: CHAR VALUES
+
+    //NOTE: INT VALUES
+
+    //NOTE: SHORT INT VALUES
+
+    //NOTE: LONG INT VALUES
+
+    //NOTE: FLOAT VALUES
+
+    //NOTE: DOUBLE VALUES
+
+    //NOTE: LONG DOUBLE VALUES
+
+    //NOTE: LONG LONG VALUES
     return 0;
 }
