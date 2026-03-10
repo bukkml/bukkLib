6ed61b9ad9d0470be268b8c40fb2370ddb829e81
bukk <mak.kurlovich@gmail.com>
2026-03-11 02:11:53 +0700

    kata: Vertical histogram of words counter

---
+++ kata/histograms/words_lenght/vertical.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define IN 1
+#define OUT 0
+#define FLAG int
+
+int main() {
+    int max_lenght = 100;
+
+    int* words_lenght = (int*)malloc(sizeof(int) * max_lenght);
+    if (words_lenght == NULL) {
+        printf("Malloc failed\n");
+        return 1;
+    }
+
+    int c, wlenght, max_used_lenght;
+    FLAG state;
+
+    wlenght = max_used_lenght = 0;
+    state = OUT;
+    for (int i = 0; i < max_lenght; i++) {
+        words_lenght[i] = 0;
+    }
+    while ((c = getchar()) != EOF) {
+        if (state == OUT) {
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                state = IN;
+            }
+        }
+        if (state == IN) {
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                wlenght++;
+            }
+            else {
+                if (wlenght >= max_lenght) {
+                    int old_max_lenght = max_lenght;
+                    max_lenght = wlenght + 1;
+                    int *tmp = realloc(words_lenght, sizeof(int) * max_lenght);
+                    if (tmp == NULL) {
+                        free(words_lenght);
+                        printf("Realloc failed\n");
+                        return 1;
+                    }
+                    words_lenght = tmp;
+                    for (int i = old_max_lenght; i < max_lenght; i++) {
+                        words_lenght[i] = 0;
+                    }
+                }
+                words_lenght[wlenght]++;
+                if (words_lenght[wlenght] > max_used_lenght)
+                    max_used_lenght = words_lenght[wlenght];
+                state = OUT;
+                wlenght = 0;
+            }
+        }
+    }
+
+    for (int i = max_used_lenght; i > 0; i--) {
+        for (int j = 0; j < max_lenght; j++) {
+            if (words_lenght[j] != 0) {
+                if (words_lenght[j] >= i) {
+                    printf("#   ");
+                }
+                else
+                    printf("    ");
+            }
+        }
+        printf("\n");
+    }
+    for (int j = 0; j < max_lenght; j++) {
+        if(words_lenght[j] > 0) {
+            printf("%d   ", j);
+        }
+    }
+    printf("\n");
+
+    free(words_lenght);
+    return 0;
+}
