550a6261b551a037250ab9c3f2eaf1921971cf00
bukk <mak.kurlovich@gmail.com>
2026-03-15 19:48:07 +0700

    kata: Print lines that have more than 80 characters

---
+++ kata/80char/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define LINELEN 80
+#define MAXLEN 1000
+#define MAXLINES 10
+
+int getline_(char line[MAXLEN]) {
+    int c, i;
+    for (i= 0; i < MAXLEN - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        line[i] = c;
+    }
+    if (c == '\n') {
+        line[i] = c;
+        ++i;
+    }
+
+    line[i] = '\0';
+    return i;
+}
+
+void copy(char to[MAXLEN], char from[MAXLEN]) {
+    int i = 0;
+    while ((to[i] = from[i]) != '\0') {
+        i++;
+    }
+}
+
+int main() {
+    char line[MAXLEN];
+    char answer[MAXLINES][MAXLEN];
+    int len, i;
+    i = 0;
+    while ((len = getline_(line)) > 0) {
+        if (len >= 80) {
+            copy(answer[i], line);
+            i++;
+        }
+    }
+
+    for (int j = 0; j < i; j++) {
+        printf("%s", answer[j]);
+    }
+
+    return 0;
+}
